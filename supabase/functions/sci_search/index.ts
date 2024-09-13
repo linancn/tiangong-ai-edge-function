9cedf30d1132779ee6e4896ba30742baf32071d5
Nan <linanenv@gmail.com>
2024-09-13 16:26:38 +0800

    Refactor authorization header in sci_search function

--- supabase/functions/sci_search/index.ts
+++ supabase/functions/sci_search/index.ts
@@ -201,7 +201,6 @@ Deno.serve(async (req) => {
   2. Make an HTTP request:
 
   curl -i --location --request POST 'http://127.0.0.1:64321/functions/v1/sci_search' \
-     --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
     --header 'Content-Type: application/json' \
     --header 'email: xxx' \
     --header 'password: xxx' \
